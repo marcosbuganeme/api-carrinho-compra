e30e5b97db1213395fccef73ff01887af508fc82
Marcos Buganeme <marcos.after@gmail.com>
2019-04-12 18:19:57 -0300

    add pesquisa de email de cliente

--- src/main/java/api/carrinho/compra/domain/repository/ClienteRepository.java
+++ src/main/java/api/carrinho/compra/domain/repository/ClienteRepository.java
@@ -1,9 +1,12 @@
 package api.carrinho.compra.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import api.carrinho.compra.domain.model.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
+	Optional<Cliente> findByEmail(String email);
 }
