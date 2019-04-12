b0064d616fb26970a7a784d8ff5a18beee60482c
Marcos Buganeme <marcos.after@gmail.com>
2019-04-12 13:36:33 -0300

    configuração inicial do banco de dados
    Adicionando carga de dados e configurando o banco de dados (mysql)

---
+++ src/main/resources/data-initialize.sql
@@ -0,0 +1,11 @@
+INSERT INTO cliente(id, nome, email) VALUES (1, 'marcos olavo', 'marcos.after@gmail.com');
+INSERT INTO cliente(id, nome, email) VALUES (2, 'emilly marcal', 'emillymarcal@gmail.com');
+
+INSERT INTO produto(id, nome, preco) VALUES (1, 'camisa de corrida oxer', 55.13);
+INSERT INTO produto(id, nome, preco) VALUES (2, 'tenis nike shoks extreme', 543.02);
+INSERT INTO produto(id, nome, preco) VALUES (3, 'teclado wireless microsoft', 155.00);
+INSERT INTO produto(id, nome, preco) VALUES (4, 'mouse naga razer', 333.00);
+INSERT INTO produto(id, nome, preco) VALUES (5, 'caixa de som jbl 40W', 220.20);
+INSERT INTO produto(id, nome, preco) VALUES (6, 'xícara de porcelanato', 32.06);
+INSERT INTO produto(id, nome, preco) VALUES (7, 'cama de casal box', 275.00);
+INSERT INTO produto(id, nome, preco) VALUES (8, 'relógio armani essentials power man', 2445.05);
