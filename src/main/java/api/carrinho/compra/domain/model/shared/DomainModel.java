c60d41513f99f9fe1f72bd4a73457f106a2f81d7
Marcos Buganeme <marcos.after@gmail.com>
2019-04-29 10:07:14 -0300

    equals e hashcode na abstração do domínio

--- src/main/java/api/carrinho/compra/domain/model/shared/DomainModel.java
+++ src/main/java/api/carrinho/compra/domain/model/shared/DomainModel.java
@@ -19,4 +19,28 @@ public abstract class DomainModel<ID> {
 	public void setId(ID id) {
 		this.id = id;
 	}
+
+	public @Override int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public @Override boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		@SuppressWarnings("rawtypes")
+		DomainModel other = (DomainModel) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
