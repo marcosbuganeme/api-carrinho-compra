dc389b3c74fe03f5c447b9b9e8858d73a535356e
Marcos Buganeme <marcos.after@gmail.com>
2019-04-30 18:17:34 -0300

    refatorando serviço de carrinho de compra

--- src/main/java/api/carrinho/compra/domain/service/CarrinhoCompraService.java
+++ src/main/java/api/carrinho/compra/domain/service/CarrinhoCompraService.java
@@ -1,13 +1,43 @@
 package api.carrinho.compra.domain.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
+
+import api.carrinho.compra.domain.model.ItemPedido;
 
 @Service
 @SessionScope
 public class CarrinhoCompraService {
 
-	public void removerItem() {
-		
+	private List<ItemPedido> itens;
+
+	{
+		itens = new ArrayList<>(5);
+	}
+
+	public void adicionarItem(ItemPedido item) {
+
+		validaAndExecuta(item, itens::contains, itens::add);
+	}
+
+	public void removerItem(ItemPedido item) {
+
+		validaAndExecuta(item, itens::contains, itens::remove);
+	}
+
+	private <T> void validaAndExecuta(T objetoDominio, 
+									  Predicate<T> condicao, 
+									  Consumer<T> acaoExecutada) {
+
+		Optional
+			.ofNullable(objetoDominio)
+			.filter(condicao)
+			.ifPresent(acaoExecutada);
 	}
 }
